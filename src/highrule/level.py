73fd4ce50d75ab2c19a607dbe385ad26d18cbc38
David Nicholson <nickledave@users.noreply.github.com>
2022-12-29 20:06:33 -0500

    Add create_map method and initial run method to Level class

--- src/highrule/level.py
+++ src/highrule/level.py
@@ -1,11 +1,30 @@
 import pygame
 
+from . import settings
+from .player import Player
+from .tile import Tile
+
 
 class Level:
     def __init__(self):
+        self.display_surface = pygame.display.get_surface()
 
         self.visible_sprites = pygame.sprite.Group()
         self.obstacle_sprites = pygame.sprite.Group()
 
+        self.create_map()
+
+    def create_map(self):
+        for row_index, row in enumerate(settings.WORLD_MAP):
+            for col_index, col_char in enumerate(row):
+                x = col_index * settings.TILESIZE
+                y = row_index * settings.TILESIZE
+                if col_char == 'x':
+                    Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
+                elif col_char == 'p':
+                    Player((x, y), [self.visible_sprites])
+
     def run(self):
-        pass
+        self.visible_sprites.draw(
+            self.display_surface
+        )
