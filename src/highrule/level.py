78c8b79d913d6d38b62d77ec9ed6e7b49941ce2f
David Nicholson <nickledave@users.noreply.github.com>
2022-12-29 22:21:02 -0500

    Add YSortCameraGroup to level.py, use in Level

--- src/highrule/level.py
+++ src/highrule/level.py
@@ -5,11 +5,34 @@ from .player import Player
 from .tile import Tile
 
 
+class YSortCameraGroup(pygame.sprite.Group):
+    """Camera that centers player on screen
+    by drawing other sprites offset from its position.
+
+    Also sorts sprites by y-coordinate
+    so sprites higher on the screen are "behind"
+    sprites lower on screen.
+    """
+    def __init__(self):
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_size()[0] // 2
+        self.half_height = self.display_surface.get_size()[1] // 2
+        self.offset = pygame.math.Vector2()
+
+    def custom_draw(self, player):
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
+
+        for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
+            offset_pos = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image, offset_pos)
+
 class Level:
     def __init__(self):
         self.display_surface = pygame.display.get_surface()
 
-        self.visible_sprites = pygame.sprite.Group()
+        self.visible_sprites = YSortCameraGroup()
         self.obstacle_sprites = pygame.sprite.Group()
 
         self.create_map()
@@ -22,9 +45,9 @@ class Level:
                 if col_char == 'x':
                     Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
                 elif col_char == 'p':
-                    Player((x, y), [self.visible_sprites])
+                    self.player = Player((x, y), [self.visible_sprites], self.obstacle_sprites)
 
     def run(self):
-        self.visible_sprites.draw(
-            self.display_surface
-        )
+        self.visible_sprites.custom_draw(self.player)
+        self.visible_sprites.update()
+
