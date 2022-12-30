f36701fc5ca89d2e71538d3d19d6b8b301e08055
David Nicholson <nickledave@users.noreply.github.com>
2022-12-29 20:40:55 -0500

    Remove debug from main.py
    args + usage in Game.run

--- src/highrule/main.py
+++ src/highrule/main.py
@@ -7,20 +7,17 @@ from . import (
 )
 from .debug import debug
 from .level import Level
-from .parser import get_parser
 
 
 class Game:
     """Class that represent ``highrule`` game"""
-    def __init__(self, debug=False):
+    def __init__(self):
 
         pygame.init()
         self.screen = pygame.display.set_mode((settings.WIDTH, settings.HEIGHT))
         pygame.display.set_caption('Caves of High Rule')
         self.clock = pygame.time.Clock()
         self.level = Level()
-
-        self.debug = debug
 
     def run(self):
         """Method that runs entire game"""
@@ -33,20 +30,15 @@ class Game:
             self.screen.fill('black')
             self.level.run()
 
-            if self.debug:
-                debug('write debug info here.')
-
             pygame.display.update()
             self.clock.tick(settings.FPS)
 
 
-def main(debug=False):
+def main():
     """Main function, used as entry point when highrule is run from the command line"""
-    game = Game(debug=debug)
+    game = Game()
     game.run()
 
 
 if __name__ == '__main__':
-    parser = get_parser()
-    args = parser.parse_args()
-    main(debug=args.debug)
+    main()
