962b5878e4e896be2a6aca19112c83a03742f785
David Nicholson <nickledave@users.noreply.github.com>
2022-12-29 22:20:29 -0500

    Finish Player class, use hitbox when moving

--- src/highrule/player.py
+++ src/highrule/player.py
@@ -4,12 +4,16 @@ from . import settings
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, groups):
+    def __init__(self, pos, groups, obstacle_sprites):
         super().__init__(groups)
         self.image = pygame.image.load(settings.GRAPHICS_ROOT / 'test/player.png').convert_alpha()
         self.rect = self.image.get_rect(topleft=pos)
+        self.hitbox = self.rect.inflate(0, -26)
 
         self.direction = pygame.math.Vector2()
+        self.speed = 5
+
+        self.obstacle_sprites = obstacle_sprites
 
     def input(self):
         keys = pygame.key.get_pressed()
@@ -28,5 +32,33 @@ class Player(pygame.sprite.Sprite):
         else:
             self.direction.x = 0
 
+    def move(self, speed):
+        if self.direction.magnitude() != 0:
+            self.direction = self.direction.normalize()
+
+        self.hitbox.x += self.direction.x * speed
+        self.collision('horizontal')
+        self.hitbox.y += self.direction.y * speed
+        self.collision('vertical')
+        self.rect.center = self.hitbox.center
+
+    def collision(self, direction):
+        if direction == 'horizontal':
+            for obstacle_sprite in self.obstacle_sprites:
+                if obstacle_sprite.hitbox.colliderect(self.hitbox):
+                    if self.direction.x > 0:  # moving right
+                        self.hitbox.right = obstacle_sprite.hitbox.left
+                    if self.direction.x < 0:
+                        self.hitbox.left = obstacle_sprite.hitbox.right
+
+        if direction == 'vertical':
+            for obstacle_sprite in self.obstacle_sprites:
+                if obstacle_sprite.hitbox.colliderect(self.hitbox):
+                    if self.direction.y > 0:  # moving down
+                        self.hitbox.bottom = obstacle_sprite.hitbox.top
+                    if self.direction.y < 0:
+                        self.hitbox.top = obstacle_sprite.hitbox.bottom
+
     def update(self):
         self.input()
+        self.move(self.speed)
