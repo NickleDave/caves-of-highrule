450ca9ecceb8671089a4d8ae3163011f296f8779
David Nicholson <nickledave@users.noreply.github.com>
2022-12-30 08:48:40 -0500

    Add src/highrule/folder.py

---
+++ src/highrule/folder.py
@@ -0,0 +1,16 @@
+from os import walk
+import pathlib
+
+import pygame
+
+
+def img_folder_to_surfaces(folder_path):
+    surface_list = []
+
+    for _, _, img_files in walk(folder_path):
+        for img_file in img_files:
+            img_path = pathlib.Path(folder_path) / img_file
+            img_surface = pygame.image.load(img_path).convert_alpha()
+            surface_list.append(img_surface)
+
+    return surface_list
