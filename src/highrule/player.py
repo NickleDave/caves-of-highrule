96cc58cfe0a944e27463bf6bf2111288b9085fd2
David Nicholson <nickledave@users.noreply.github.com>
2022-12-29 20:04:19 -0500

    Use GRAPHICS_ROOT in player.py

--- src/highrule/player.py
+++ src/highrule/player.py
@@ -6,5 +6,5 @@ from . import settings
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
-        self.image = pygame.image.load('./graphics/test/player.png').convert_alpha()
+        self.image = pygame.image.load(settings.GRAPHICS_ROOT / 'test/player.png').convert_alpha()
         self.rect = self.image.get_rect(topleft=pos)
