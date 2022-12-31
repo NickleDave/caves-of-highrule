9dfc0c1736394b7eae15518312bfaa781d0e0de3
David Nicholson <nickledave@users.noreply.github.com>
2022-12-30 23:07:59 -0500

    Sort img_files in img_folder_to_surfaces function

--- src/highrule/folder.py
+++ src/highrule/folder.py
@@ -8,6 +8,7 @@ def img_folder_to_surfaces(folder_path):
     surface_list = []
 
     for _, _, img_files in walk(folder_path):
+        img_files = sorted(img_files)
         for img_file in img_files:
             img_path = pathlib.Path(folder_path) / img_file
             img_surface = pygame.image.load(img_path).convert_alpha()
