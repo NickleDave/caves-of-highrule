5f1b7282220c7ee50297a54d0d330adc14461df2
David Nicholson <nickledave@users.noreply.github.com>
2022-12-31 00:29:24 -0500

    WIP: Add animations to player

--- src/highrule/player.py
+++ src/highrule/player.py
@@ -1,6 +1,9 @@
 import pygame
 
-from . import settings
+from . import (
+    folder,
+    settings
+)
 
 
 class Player(pygame.sprite.Sprite):
@@ -10,27 +13,72 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(topleft=pos)
         self.hitbox = self.rect.inflate(0, -26)
 
+        self.import_player_assets()
+        self.status = 'down'
+
+        # movement
         self.direction = pygame.math.Vector2()
         self.speed = 5
+        self.attacking = False
+        self.attack_cooldown = 400
+        self.attack_time = None
 
         self.obstacle_sprites = obstacle_sprites
+
+    def import_player_assets(self):
+        character_path = settings.GRAPHICS_ROOT / 'player'
+        self.animations = {}
+        for movement_type in [
+            'down',
+            'down_attack'
+            'down_idle',
+            'left',
+            'left_attack',
+            'left_idle',
+            'right',
+            'right_attack',
+            'right_idle',
+            'up',
+            'up_attack',
+            'up_idle',
+            ]:
+            self.animations[movement_type] = folder.img_folder_to_surfaces(character_path / movement_type)
 
     def input(self):
         keys = pygame.key.get_pressed()
 
+        # ---- movement --------------------
         if keys[pygame.K_UP]:
             self.direction.y = -1
+            self.status = 'up'
         elif keys[pygame.K_DOWN]:
             self.direction.y = 1
+            self.status = 'down'
         else:
             self.direction.y = 0
 
         if keys[pygame.K_RIGHT]:
             self.direction.x = 1
+            self.stauts = 'right'
         elif keys[pygame.K_LEFT]:
             self.direction.x = -1
+            self.direction = 'left'
         else:
             self.direction.x = 0
+
+        # ---- attack --------------------
+        if keys[pygame.K_SPACE] and not self.attacking:
+            self.attacking = True
+            self.attack_time = pygame.time.get_ticks()
+
+        # ---- magic --------------------
+        if keys[pygame.K_LCTRL] and not self.attacking:
+            self.attacking = True
+            self.attack_time = pygame.time.get_ticks()
+
+    def get_status(self):
+        if self.direction.x == 0 and self.direction.y == 0:
+            self.status = f'{self.status}_idle'
 
     def move(self, speed):
         if self.direction.magnitude() != 0:
@@ -59,6 +107,15 @@ class Player(pygame.sprite.Sprite):
                     if self.direction.y < 0:
                         self.hitbox.top = obstacle_sprite.hitbox.bottom
 
+    def cooldown(self):
+        current_time = pygame.time.get_ticks()
+
+        if self.attacking:
+            if current_time - self.attack_time >= self.attack_cooldown:
+                self.attacking = False
+
     def update(self):
         self.input()
+        self.cooldown()
+        self.get_status()
         self.move(self.speed)
